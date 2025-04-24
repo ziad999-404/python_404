af255d23953fef7833757077b56ed53704af8d6d
ziad999-404 <ziad_shalaby@outlook.com>
2025-04-24 05:52:32 -0700

    Add files via upload

---
+++ task.py
@@ -0,0 +1,16 @@
+# this code is made by eng_ziad /:
+# temperature_task :
+
+temperature = int(input("Enter temperature in celsius : "))
+    ##
+if temperature >= 30 :
+    print("it's a hot day . stay hydrated")
+    ##
+elif temperature >= 20 and temperature <= 29 :
+    print("it's a warm day . enjoy the weather")
+    ##
+elif temperature >= 10 and temperature <=19 :
+    print("it's a cold day . wear a jacket")
+    ##
+else :
+    print ("it's a cold day . stay warm")
---
+++ task_2.0.py
@@ -0,0 +1,3 @@
+for x in range(1 , 21) :
+    if x%3 == 0 :
+        print(x)
